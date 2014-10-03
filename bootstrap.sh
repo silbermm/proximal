7cd9185a7d88d96ae9f6dd697c46f6128c93649b
silbermm <silbermm@gmail.com>
2014-10-02 23:08:32 -0400

    Add git to vagrant shell provisioner

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ apt-get install -y python-software-properties
 apt-add-repository -y ppa:chris-lea/node.js
 apt-get update
 apt-get install -y openjdk-7-jdk
+apt-get install -y git
 apt-get install -y nodejs
 npm install -g grunt-cli
 npm install -g bower
