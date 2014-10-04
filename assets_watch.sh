53ee711ada132bca929ceec99167b0c1d64bd8b8
silbermm <silbermm@gmail.com>
2014-10-03 21:24:45 -0400

    fix for when npm install fails

--- assets_watch.sh
+++ assets_watch.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-su - vagrant -c 'cd /vagrant && bower install && npm install && grunt watchd'
+su - vagrant -c 'cd /vagrant && bower install'
+su - vagrant -c 'cd /vagrant && npm cache clean'
+su - vagrant -c 'cd /vagrant && npm install grunt-contrib-imagemin'
+su - vagrant -c 'cd /vagrant && npm install'
 #su - vagrant -c 'bower install' -p
 #su - vagrant -c 'npm install' -p
 #su - vagrant -c 'grunt watchd' -p
