2363577135c7eca7c79e96e9b39bf4afbc8792e4
silbermm <silbermm@gmail.com>
2015-04-10 20:56:37 -0400

    Added image for activities

--- web/admin/admin_controller.js
+++ web/admin/admin_controller.js
@@ -2,14 +2,15 @@
 
   var ccStandards = require("../images/ccStandards.jpg");
   var resourceImg = require("../images/resources.png");
-    
+  var activityImg = require("../images/activities.jpeg");    
   module.exports = function($log,$cookieStore, standardsService, $modal){
     var vm = this; 
     vm.page = "Admin Page";
 
     vm.ccImage = ccStandards;
     vm.resourceImage = resourceImg;
-   
+    vm.activityImage = activityImg; 
+
     vm.createStandard = function() {
       modalInstance = $modal.open({
         templateUrl: 'standards/add_standard.html',
